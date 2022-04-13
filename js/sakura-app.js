d464615663312ee49c5678b33aaced9e7165d595
qingyiyu <2418730499@qq.com>
2022-04-13 22:19:10 +0800

    Site updated: 2022-04-13 22:19:07

--- js/sakura-app.js
+++ js/sakura-app.js
@@ -252,7 +252,7 @@ function code_highlight_style () {
       'autocapitalize': 'off',
       'spellcheck': 'false',
       'contenteditable': 'false',
-      'design': 'by hojun'
+      'design': 'by qingyiyu'
     }
     var ele_name = $('pre:eq(' + i + ')')[0].children[0].className
     var lang = ele_name.substr(0, ele_name.indexOf(' ')).replace('language-', '')
@@ -1610,8 +1610,8 @@ $(function () {
     $(this).postLike()
   })
   console.log('%c Mashiro %c', 'background:#24272A; color:#ffffff', '', 'https://2heng.xin/')
-  console.log('%c hojun %c', 'background:#24272A; color:#ffffff', '', 'https://www.hojun.cn/')
-  console.log('%c Github %c', 'background:#24272A; color:#ffffff', '', 'https://github.com/honjun/hexo-theme-sakura')
+  console.log('%c qingyiyu %c', 'background:#24272A; color:#ffffff', '', 'https://qingyiyu.github.io/')
+  console.log('%c Github %c', 'background:#24272A; color:#ffffff', '', 'https://github.com/qingyiyu/hexo-theme-sakura')
 })
 var isWebkit = navigator.userAgent.toLowerCase().indexOf('webkit') > -1,
   isOpera = navigator.userAgent.toLowerCase().indexOf('opera') > -1,
